49f728b58e4d83f45b8d1c46f0f9274efd47edee
Boji . <developerboji@gmail.com>
2024-10-20 08:23:36 +0700

    Export all from users_bestplay.ts
    Co-Authored-By: Andhika Dwi Maulana <andhkdwmln@gmail.com>

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -3,3 +3,4 @@ export * from './users/users_id';
 export * from './users/users_data';
 export * from './users/users_recentplay';
 export * from './users/users_firstplace';
+export * from './users/users_bestplay';
